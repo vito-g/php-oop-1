545bff72305743ccdcf199735e72f519ab3c95d6
Vito Gentile <gentilevito1076@gmail.com>
2021-03-15 16:29:06 +0100

    Creazione Oggetto PLatone

--- index.php
+++ index.php
@@ -1,24 +1,32 @@
 <?php
 
 class Vino {
-    public $nazione;
+  public $nazione;
 
-    public $regione;
+  public $regione;
 
-    public $zonaProduzione;
+  public $zonaProduzione;
 
-    public $indicazione;
+  public $indicazione;
 
-    public $tipologia;
+  public $tipologia;
 
-    public $vitigno;
+  public $vitigno;
 
-    public function __construct($nazione, $regione, $zonaProduzione, $indicazione, $tipologia, $vitigno) {
-      $this->nazione = $nazione;
-      $this->regione = $regione;
-      $this->zonaProduzione = $zonaProduzione;
-      $this->indicazione = $indicazione;
-      $this->tipologia = $tipologia;
-      $this->vitigno = $vitigno;
-    }
+  public function __construct($nazione, $regione, $zonaProduzione, $indicazione, $tipologia, $vitigno) {
+    $this->nazione = $nazione;
+    $this->regione = $regione;
+    $this->zonaProduzione = $zonaProduzione;
+    $this->indicazione = $indicazione;
+    $this->tipologia = $tipologia;
+    $this->vitigno = $vitigno;
   }
+}
+
+$Platone = new Vino('Italia', 'Puglia', 'Cellino San Marco', 'IGP Salento', 'Vino Rosso', '50% Primitivo 50% Negroamaro');
+echo $Platone->nazione . '<br>';
+echo $Platone->regione . '<br>';
+echo $Platone->zonaProduzione . '<br>';
+echo $Platone->indicazione . '<br>';
+echo $Platone->tipologia . '<br>';
+echo $Platone->vitigno . '<br>';
