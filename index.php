43ca505a7633a6f82eeb01256222a9a505d6902e
Vito Gentile <gentilevito1076@gmail.com>
2021-03-15 16:42:49 +0100

    Creazione Oggetti Mediterrano e Felicità

--- index.php
+++ index.php
@@ -40,3 +40,23 @@ echo $Taras->zonaProduzione . '<br>';
 echo $Taras->indicazione . '<br>';
 echo $Taras->tipologia . '<br>';
 echo $Taras->vitigno . '<br>';
+
+echo '<br>';
+
+$Mediterraneo = new Vino('Italia', 'Puglia', 'Cellino San Marco', 'IGP Salento', 'Vino Rosato', 'Negramaro');
+echo $Mediterraneo->nazione . '<br>';
+echo $Mediterraneo->regione . '<br>';
+echo $Mediterraneo->zonaProduzione . '<br>';
+echo $Mediterraneo->indicazione . '<br>';
+echo $Mediterraneo->tipologia . '<br>';
+echo $Mediterraneo->vitigno . '<br>';
+
+echo '<br>';
+
+$Felicità = new Vino('Italia', 'Puglia', 'Cellino San Marco', 'IGP Salento', 'Vino Bianco', '70% Sauvignon 30% Chardonnay');
+echo $Felicità->nazione . '<br>';
+echo $Felicità->regione . '<br>';
+echo $Felicità->zonaProduzione . '<br>';
+echo $Felicità->indicazione . '<br>';
+echo $Felicità->tipologia . '<br>';
+echo $Felicità->vitigno . '<br>';
